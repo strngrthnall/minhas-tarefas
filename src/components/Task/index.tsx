919d0ca6abe683a92083eaa91a3deb77cab21176
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2024-02-17 12:45:24 -0300

    Componentização dos itens do card

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Task = () => (
-  <S.Card>
-    <S.Title>Nome da Tarefa</S.Title>
-    <S.Tag>importante</S.Tag>
-    <S.Tag>pendente</S.Tag>
-    <S.Description />
-    <S.ActionBar>
-      <S.Button>Editar</S.Button>
-      <S.Button>Remover</S.Button>
-    </S.ActionBar>
-  </S.Card>
-)
+type Props = {
+  title: string
+  priority: string
+  stats: string
+  description: string
+}
+
+const Task = ({ title, priority, stats, description }: Props) => {
+  const [editing, setEditing] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Title>{title}</S.Title>
+      <S.Tag>{priority}</S.Tag>
+      <S.Tag>{stats}</S.Tag>
+      <S.Description value={description} />
+      <S.ActionBar>
+        {editing ? (
+          <>
+            <S.Button>Salvar</S.Button>
+            <S.Button onClick={() => setEditing(false)}>Cancelar</S.Button>
+          </>
+        ) : (
+          <>
+            <S.Button onClick={() => setEditing(true)}>Editar</S.Button>
+            <S.Button>Remover</S.Button>
+          </>
+        )}
+      </S.ActionBar>
+    </S.Card>
+  )
+}
 
 export default Task
--- src/containers/ToDoList/index.tsx
+++ src/containers/ToDoList/index.tsx
@@ -2,25 +2,41 @@ import Task from '../../components/Task'
 
 import * as S from './styles'
 
+const tasks = [
+  {
+    title: 'Estudar Typescript',
+    description: 'Ver aula 3 da EBAC',
+    priority: 'importante',
+    stats: 'pendente'
+  },
+  {
+    title: 'Pagar conta da Internet',
+    description: 'Baixar fatura no  email',
+    priority: 'urgente',
+    stats: 'concluida'
+  },
+  {
+    title: 'Ir para academia',
+    description: 'Fazer treino B',
+    priority: 'importante',
+    stats: 'pendente'
+  }
+]
+
 const ToDoList = () => (
   <S.Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
+      {tasks.map((t) => (
+        <li key={t.title}>
+          <Task
+            title={t.description}
+            priority={t.priority}
+            stats={t.stats}
+            description={t.description}
+          />
+        </li>
+      ))}
     </ul>
   </S.Container>
 )
